3738109ba430fcb2198eafd8fa1fff54ae973e9f
Aleksandr Lundin <30766000+alexlundin@users.noreply.github.com>
2022-07-25 12:49:01 +0300

    first commit

---
+++ includes/class-asl-battle-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://vk.com/aslundin
+ * @since      1.0.0
+ *
+ * @package    Asl_Battle
+ * @subpackage Asl_Battle/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Asl_Battle
+ * @subpackage Asl_Battle/includes
+ * @author     Alex Lundin <aslundin@yandex.ru>
+ */
+class Asl_Battle_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-asl-battle-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://vk.com/aslundin
+ * @since      1.0.0
+ *
+ * @package    Asl_Battle
+ * @subpackage Asl_Battle/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Asl_Battle
+ * @subpackage Asl_Battle/includes
+ * @author     Alex Lundin <aslundin@yandex.ru>
+ */
+class Asl_Battle_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'asl-battle',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
